d59257efd34306d4639863883ada353a751131ab
ansar <mohammedansarusman@gmail.com>
2025-11-13 22:56:52 +0400

    wip nav bar and mobile nav bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        classNameName={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NavigationBar />
         {children}
